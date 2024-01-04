32b45aeed23609932e71f785ff0cbbca43cd6dab
Fractal-Tess <vgfractal@gmail.com>
2024-01-04 17:40:21 +0200

    refactor client file

--- apps/web/src/hooks.client.ts
+++ apps/web/src/hooks.client.ts
@@ -1,23 +1,32 @@
 import { directus } from '$lib/directus'
+import { asyncTryOrElse } from '$lib/utils'
 
-try {
-  const authRes = await fetch(
-    'https://directus-jet-black.app.jet-black.xyz/auth/refresh',
-    {
-      method: 'POST',
-      credentials: 'include',
-      body: JSON.stringify({
-        mode: 'cookie'
-      }),
-      headers: {
-        'Content-Type': 'application/json'
+async function refreshToken() {
+  const res = await asyncTryOrElse(
+    async () =>
+      await fetch('https://directus-jet-black.app.jet-black.xyz/auth/refresh', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({
+          mode: 'cookie'
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+  )
+  if (res.isOk) {
+    if (res.value.status !== 200) return
+    const {
+      data: { access_token }
+    } = (await res.value.json()) as {
+      data: {
+        access_token: string
+        expires: number
       }
     }
-  )
-  type Auth = {
-    access_token: string
-    expires: number
+    directus.setToken(access_token)
   }
-  const data = (await authRes.json()) as Auth
-  directus.setToken(data.access_token)
-} catch (error) {}
+}
+await refreshToken()
+setInterval(refreshToken, 1000 * 60 * 10)
