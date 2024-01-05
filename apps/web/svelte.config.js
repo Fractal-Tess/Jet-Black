e4d031f2e07c139b5247ced36759003520395ef7
Fractal-Tess <vgfractal@gmail.com>
2024-01-05 18:59:02 +0200

    add mdsvex

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,13 +1,17 @@
 import adapter from '@sveltejs/adapter-node'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { mdsvex } from 'mdsvex'
+import mdsvexConfig from './mdsvex.config.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte'],
+  extensions: ['.svelte', '.svx'],
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [vitePreprocess()],
-
+  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+  compilerOptions: {
+    customElement: true
+  },
   vitePlugin: {
     inspector: true
   },
@@ -16,6 +20,10 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter()
+  },
+  onwarn: (warning, handler) => {
+    if (warning.code === 'css-unused-selector') return
+    handler(warning)
   }
 }
 export default config
