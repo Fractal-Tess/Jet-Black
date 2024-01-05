7751944a4a370bf40e91d235f5df1b3f8b155556
Fractal-Tess <vgfractal@gmail.com>
2024-01-05 19:02:46 +0200

    refactor

--- apps/web/src/routes/(auth)/+layout.svelte
+++ apps/web/src/routes/(auth)/+layout.svelte
@@ -2,16 +2,16 @@
   import { fade } from 'svelte/transition'
 </script>
 
-<main class="flex items-center justify-between flex-col pt-8 min-h-screen">
+<main class="flex items-center justify-between flex-col pt-8 min-h-screen pb-4">
   <a href="/" class="block p-4">
-    <img src="/jb/Jet-Black.svg" alt="Logo" class="h-20 md:h-24 invert" />
+    <img src="/logo_white.svg" alt="Logo" class="h-20 md:h-24" />
   </a>
 
-  <section class="container min-h-full md:max-w-sm max-w-xs" transition:fade>
+  <section class="container max-w-sm md:max-w-md" transition:fade>
     <slot />
   </section>
 
-  <footer class="flex items-center justify-center gap-x-2 text-sm mb-4">
+  <footer class="flex gap-x-2 text-sm">
     <a href="/privacy-polcy"> Политика за поверителност </a>
     <span>|</span>
     <a href="/terms"> Условия за ползване</a>
