7535883c2e5972accdc51e932d14f3311007e782
Fractal-Tess <vgfractal@gmail.com>
2024-01-05 19:00:07 +0200

    reduce config

--- apps/web/src/lib/directus/index.ts
+++ apps/web/src/lib/directus/index.ts
@@ -1,31 +1,14 @@
-import { authentication, createDirectus, readItems, rest } from '@directus/sdk'
+import { authentication, createDirectus, rest } from '@directus/sdk'
 import { env } from '$env/dynamic/public'
 import type { Schema } from './schema'
 export * from './schema'
 export * from './utils'
 
-let token = ''
-
 export const directus = createDirectus<Schema>(env.PUBLIC_DIRECTUS_URL)
-  .with(rest())
   .with(
     authentication('cookie', {
       autoRefresh: true,
-      credentials: 'include',
-      storage: {
-        get: async () => {
-          console.log('Calling get')
-          return {
-            access_token: token,
-            refresh_token: null,
-            expires: null,
-            expires_at: null
-          }
-        },
-
-        set: async value => {
-          console.log('Calling set')
-        }
-      }
+      credentials: 'include'
     })
   )
+  .with(rest())
