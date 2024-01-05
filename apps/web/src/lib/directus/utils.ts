3d56cc73aa4cb1831c85b2b3760ade0ba4e0eb2f
Fractal-Tess <vgfractal@gmail.com>
2024-01-05 19:00:51 +0200

    new helpers

--- apps/web/src/lib/directus/utils.ts
+++ apps/web/src/lib/directus/utils.ts
@@ -1,4 +1,47 @@
 import { env } from '$env/dynamic/public'
-export function directusAsset(asset: string) {
+import { asyncTryOrElse } from '$lib/utils'
+import { readItems, readMe } from '@directus/sdk'
+import { directus } from '.'
+
+export function directusAsset(asset: string | unknown) {
+  if (typeof asset !== 'string') return
   return env.PUBLIC_DIRECTUS_URL + '/assets/' + asset
+}
+
+export async function getUser() {
+  const result = await asyncTryOrElse(
+    async () => await directus.request(readMe())
+  )
+  return result.isOk ? result.value : null
+}
+
+export async function getChapters(subject: string) {
+  const tree = await directus.request(
+    readItems('chapter', {
+      // @ts-ignore
+      fields: ['*.*.*'],
+
+      filter: {
+        sub_chapters: {
+          _nnull: true
+        },
+        subject: {
+          label: {
+            _eq: subject
+          }
+        }
+      }
+    })
+  )
+
+  return tree
+}
+
+export async function getSubjects() {
+  const result = await asyncTryOrElse(
+    async () => await directus.request(readItems('subject'))
+  )
+
+  if (result.isErr) return []
+  return result.value
 }
