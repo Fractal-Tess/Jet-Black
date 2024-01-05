616a027f575a55f18fed581deba44b1904903bd0
Fractal-Tess <vgfractal@gmail.com>
2024-01-05 18:59:40 +0200

    remove hooks file

--- apps/web/src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-import type { Handle } from '@sveltejs/kit'
-
-export const handle: Handle = async ({ resolve, event }) => {
-  console.log(event.cookies.getAll())
-  const response = await resolve(event)
-
-  return response
-}
