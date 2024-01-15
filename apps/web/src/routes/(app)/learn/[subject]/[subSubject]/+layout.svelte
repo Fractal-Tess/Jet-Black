dcd5052830b7e73a26e95f38440c9cd0f24b446f
Fractal-Tess <vgfractal@gmail.com>
2024-01-15 04:40:09 +0200

    subSubject rendering

---
+++ apps/web/src/routes/(app)/learn/[subject]/[subSubject]/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { LayoutData } from './$types'
+
+  export let data: LayoutData
+  const { chapters, subSubject, subject } = data
+</script>
+
+<section class="h-full flex">
+  <aside class="h-full">
+    <ul class="h-full dev w-40 py-8">
+      {#each chapters as chapter, i}
+        <li class="dev">
+          {#each chapter.lessons as item}
+            <!-- content here -->
+          {/each}
+          <a href={`/learn/${subject}/${subSubject}/${chapter.label}/`}>
+            {chapter.label}
+          </a>
+        </li>
+        <!-- content here -->
+      {/each}
+    </ul>
+  </aside>
+  <slot />
+</section>
